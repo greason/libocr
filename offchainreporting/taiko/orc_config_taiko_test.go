a95c46ce2f249a1464ab73411842cb1b509480b9
greason <greason@126.com>
2024-12-03 14:11:04 +0800

    feat: add taiko

---
+++ offchainreporting/taiko/orc_config_taiko_test.go
@@ -0,0 +1,177 @@
+package bsc
+
+import (
+	"encoding/hex"
+	"encoding/json"
+	"fmt"
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/common/hexutil"
+	ocrConfigHelper "github.com/smartcontractkit/libocr/offchainreporting/confighelper"
+	"github.com/smartcontractkit/libocr/offchainreporting/internal/test"
+	ocrTypes "github.com/smartcontractkit/libocr/offchainreporting/types"
+	"os"
+	"path/filepath"
+	"strings"
+	"testing"
+	"time"
+)
+
+func AproOffChainAggregatorConfig(numberNodes int, target int) test.OffChainAggregatorConfig {
+	if numberNodes <= 4 {
+		fmt.Printf("insufficient number of nodes (%d) supplied for OCR, need at least 5", numberNodes)
+	}
+	s := []int{1}
+	// First node's stage already inputted as a 1 in line above, so numberNodes-1.
+	for i := 0; i < numberNodes-1; i++ {
+		s = append(s, 2)
+	}
+	// chainTypeSlowUpdates
+	// limits.MinDeltaC(10min) <= cfg.DeltaC
+	// limits.MinDeltaStage(10s) <= cfg.DeltaStage
+	// cfg.DeltaGrace < cfg.DeltaRound
+	// cfg.DeltaRound < cfg.DeltaProgress
+	// 0 < cfg.RMax && cfg.RMax < 255
+	// len(cfg.S) < 1000
+
+	var AlphaPPB = uint64(10000000)
+	var DeltaC = time.Hour * 24
+	var DeltaProgress = time.Second * 35
+	var DeltaRound = time.Second * 30
+
+	switch target {
+	case TAIKO_BTC_USD:
+		// 0.5% / 3600s
+		AlphaPPB = uint64(5000000)
+		DeltaC = time.Hour * 1
+	case TAIKO_ETH_USD:
+		// 0.5% / 3600s
+		AlphaPPB = uint64(5000000)
+		DeltaC = time.Hour * 1
+	case TAIKO_USDT_USD:
+		// 0.1% / 86400s
+		AlphaPPB = uint64(1000000)
+		DeltaC = time.Hour * 24
+	case TAIKO_USDC_USD:
+		// 0.1% / 86400s
+		AlphaPPB = uint64(1000000)
+		DeltaC = time.Hour * 24
+	case TAIKO_TAIKO_USD:
+		// 0.5% / 3600s
+		AlphaPPB = uint64(5000000)
+		DeltaC = time.Hour * 1
+	}
+
+	return test.OffChainAggregatorConfig{
+		AlphaPPB:         AlphaPPB, // 10 ^9
+		DeltaC:           DeltaC,
+		DeltaGrace:       time.Second * 12,
+		DeltaProgress:    DeltaProgress,
+		DeltaStage:       time.Second * 60,
+		DeltaResend:      time.Second * 17,
+		DeltaRound:       DeltaRound,
+		RMax:             6,
+		S:                s,
+		N:                numberNodes,
+		F:                (numberNodes - 1) / 3,
+		OracleIdentities: []ocrConfigHelper.OracleIdentityExtra{},
+	}
+}
+
+const (
+	TAIKO_BTC_USD = iota
+	TAIKO_ETH_USD
+	TAIKO_USDT_USD
+	TAIKO_USDC_USD
+	TAIKO_TAIKO_USD
+)
+
+func GetNodeConfigs(target int) []test.NodeOCRConfig {
+	nodeConfigs := make(map[int][]test.NodeOCRConfig)
+	return nodeConfigs[target]
+}
+
+func GetOffChainAggregatorConfig(target int, publicKeyPath string) test.OffChainAggregatorConfig {
+	nodeConfigs := GetNodeConfigs(target)
+
+	if len(publicKeyPath) > 0 {
+		content, _ := os.ReadFile(publicKeyPath)
+		var ocrConfigs []test.NodeOCRConfig
+		err := json.Unmarshal(content, &ocrConfigs)
+		if err != nil {
+			return test.OffChainAggregatorConfig{}
+		}
+		ocrConfigs = ocrConfigs[2:]
+		nodeConfigs = ocrConfigs
+		fmt.Printf("nodeConfigs: %v", nodeConfigs)
+	}
+	ocrConfig := AproOffChainAggregatorConfig(len(nodeConfigs), target)
+	for _, nodeConfig := range nodeConfigs {
+		// Need to convert the key representations
+		var onChainSigningAddress [20]byte
+		var configPublicKey [32]byte
+		offchainSigningAddress, err := hex.DecodeString(nodeConfig.OffChainPubKey)
+		if err != nil {
+			panic(err)
+		}
+		decodeConfigKey, err := hex.DecodeString(nodeConfig.ConfigPubKey)
+		if err != nil {
+			panic(err)
+		}
+
+		// https://stackoverflow.com/questions/8032170/how-to-assign-string-to-bytes-array
+		copy(onChainSigningAddress[:], common.HexToAddress(nodeConfig.SignAddress).Bytes())
+		copy(configPublicKey[:], decodeConfigKey)
+
+		oracleIdentity := ocrConfigHelper.OracleIdentity{
+			TransmitAddress:       common.HexToAddress(nodeConfig.TransmitAddress),
+			OnChainSigningAddress: onChainSigningAddress,
+			PeerID:                nodeConfig.PeerID,
+			OffchainPublicKey:     offchainSigningAddress,
+		}
+		oracleIdentityExtra := ocrConfigHelper.OracleIdentityExtra{
+			OracleIdentity:                  oracleIdentity,
+			SharedSecretEncryptionPublicKey: ocrTypes.SharedSecretEncryptionPublicKey(configPublicKey),
+		}
+
+		ocrConfig.OracleIdentities = append(ocrConfig.OracleIdentities, oracleIdentityExtra)
+	}
+	return ocrConfig
+}
+
+func TestEncodeOCRConfig(t *testing.T) {
+	readPublicKeyFromFIle := true
+	publicKeyPath := ""
+	if readPublicKeyFromFIle {
+		publicKeyFileName := "publicKeys_taiko_usd.json"
+		publicKeyPath = filepath.Join("/Users/greason/Documents/workspace_apro/aproOracle/apro.configs/taikoMain/publicKeys/",
+			publicKeyFileName)
+	}
+
+	ocrConfig := GetOffChainAggregatorConfig(TAIKO_TAIKO_USD, publicKeyPath)
+	signers, transmitters, threshold, encodedConfigVersion, encodedConfig, err := ocrConfigHelper.ContractSetConfigArgs(
+		ocrConfig.DeltaProgress,
+		ocrConfig.DeltaResend,
+		ocrConfig.DeltaRound,
+		ocrConfig.DeltaGrace,
+		ocrConfig.DeltaC,
+		ocrConfig.AlphaPPB,
+		ocrConfig.DeltaStage,
+		ocrConfig.RMax,
+		ocrConfig.S,
+		ocrConfig.OracleIdentities,
+		ocrConfig.F,
+	)
+	fmt.Printf("\nerr: %v\n", err)
+	fmt.Printf("\nsigners: %v\n", PrintList(signers))
+	fmt.Printf("transmitters: %v\n", PrintList(transmitters))
+	fmt.Printf("threshold: %v\n", threshold)
+	fmt.Printf("encodedConfigVersion: %v\n", encodedConfigVersion)
+	fmt.Printf("encodedConfig: %v", hexutil.Encode(encodedConfig))
+}
+
+func PrintList(content []common.Address) string {
+	txt := strings.Replace(fmt.Sprintf("%v", content), " ", "\",\"", -1)
+	txt = strings.Replace(txt, "[", "[\"", -1)
+	txt = strings.Replace(txt, "]", "\"]", -1)
+	return txt
+}
